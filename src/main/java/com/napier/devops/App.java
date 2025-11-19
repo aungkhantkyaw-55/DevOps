730bfc01439a888ae2185fa02190946fb7aeff14
ASUS VivoBook <a2k3k4ok@gmail.com>
2025-11-19 18:32:52 +0630

    Updated dockerfile #afterhalf-Lab7

--- src/main/java/com/napier/devops/App.java
+++ src/main/java/com/napier/devops/App.java
@@ -1,8 +1,6 @@
 package com.napier.devops;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class App {
@@ -122,8 +120,44 @@ public class App {
         return new ArrayList<>(); // Replace with real implementation
     }
 
-    public Employee getEmployee(int i) {
-        return null;  // Return null as a placeholder
+    public Employee getEmployee(int ID) {
+        try {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT emp_no, first_name, last_name FROM employees WHERE emp_no = " + ID;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Return new employee if valid.
+            // Check one is returned
+            if (rset.next()) {
+                Employee emp = new Employee();
+                emp.emp_no = rset.getInt("emp_no");
+                emp.first_name = rset.getString("first_name");
+                emp.last_name = rset.getString("last_name");
+                return emp;
+            } else
+                return null;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get employee details");
+            return null;
+        }
     }
-
-}
+    public void addEmployee(Employee emp) {
+            try
+            {
+                Statement stmt = con.createStatement();
+                String strUpdate =
+                        "INSERT INTO employees (emp_no, first_name, last_name, birth_date, gender, hire_date) " +
+                                "VALUES (" + emp.emp_no + ", '" + emp.first_name + "', '" + emp.last_name + "', " +
+                                "'9999-01-01', 'M', '9999-01-01')";
+                stmt.execute(strUpdate);
+            }
+            catch (Exception e)
+            {
+                System.out.println(e.getMessage());
+                System.out.println("Failed to add employee");
+            }
+        }
+    }
--- src/test/java/com/napier/devops/AppIntegrationTest.java
+++ src/test/java/com/napier/devops/AppIntegrationTest.java
@@ -14,6 +14,12 @@ public class AppIntegrationTest
     {
         app = new App();
         app.connect("localhost:33060", 30000);
+
+        Employee e = new Employee();
+        e.emp_no = 255530;
+        e.first_name = "Ronghao";
+        e.last_name = "Garigliano";
+        app.addEmployee(e);
     }
 
     @Test
@@ -24,4 +30,19 @@ public class AppIntegrationTest
         assertEquals(emp.first_name, "Ronghao");
         assertEquals(emp.last_name, "Garigliano");
     }
+
+    @Test
+    void testAddEmployee()
+    {
+        Employee emp = new Employee();
+        emp.emp_no = 500000;
+        emp.first_name = "Kevin";
+        emp.last_name = "Chalmers";
+        app.addEmployee(emp);
+        emp = app.getEmployee(500000);
+        assertEquals(emp.emp_no, 500000);
+        assertEquals(emp.first_name, "Kevin");
+        assertEquals(emp.last_name, "Chalmers");
+    }
 }
+
